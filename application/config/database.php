6d41fe7eaadf2667901e7ad56eb20de505826233
Adnan Mansoor <k102015@nu.edu.pk>
2013-02-03 19:07:11 +0500

    Revert "configuration changes"
    This reverts commit f9411e2a97e45b9e8c1b1efb764b0b1de2bcd2bf.

--- application/config/database.php
+++ application/config/database.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'tlcweb';
-$db['default']['password'] = 'tlcweb';
+$db['default']['username'] = 'root';
+$db['default']['password'] = 'root';
 $db['default']['database'] = 'tlc_portal_v1';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';

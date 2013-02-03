63beef20b19adfd7ec0bf584ea42ecd0c2982d82
Adnan Mansoor <k102015@nu.edu.pk>
2013-02-04 00:39:58 +0500

    commit Test

--- DB/alter_schema.sql
+++
@@ -1,4 +0,0 @@
--- Renamed the registration_allowed field to make it consistant with
--- the nameing convention used in other tables
--- by Ali on 3 feb, 2013
-ALTER TABLE EVENTS CHANGE registration_allowed active tinyint(1) NOT NULL;
